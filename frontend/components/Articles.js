5371b2cf5ee14fd69d8cbb11fe250ccb9ada3152
Ben Ross <mrbenross@gmail.com>
2023-12-05 15:47:25 -0600

    articlesjs-updated

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from 'react'
-import { Navigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
 import PT from 'prop-types'
 
 export default function Articles(props) {
   // ✨ where are my props? Destructure them here
+  const {
+    articles,
+    getArticles,
+    deleteArticle,
+    setCurrentArticleId,
+    currentArticleId,
+  } = props;
 
+  const navigate = useNavigate()
+  const token = window.localStorage.getItem('token')
+
+  if(!token){
+    navigate('/')
+  }
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
+    getArticles()
   })
 
   return (
@@ -18,9 +32,9 @@ export default function Articles(props) {
     <div className="articles">
       <h2>Articles</h2>
       {
-        ![].length
+        !articles.length
           ? 'No articles yet'
-          : [].map(art => {
+          : articles.map(art => {
             return (
               <div className="article" key={art.article_id}>
                 <div>
@@ -29,8 +43,8 @@ export default function Articles(props) {
                   <p>Topic: {art.topic}</p>
                 </div>
                 <div>
-                  <button disabled={true} onClick={Function.prototype}>Edit</button>
-                  <button disabled={true} onClick={Function.prototype}>Delete</button>
+                  <button disabled={!!currentArticleId} onClick={()=>setCurrentArticleId(art.article_id)}>Edit</button>
+                  <button disabled={!!currentArticleId} onClick={()=>deleteArticle(art.article_id)}>Delete</button>
                 </div>
               </div>
             )
