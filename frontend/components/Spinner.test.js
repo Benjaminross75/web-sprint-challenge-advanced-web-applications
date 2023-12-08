d6658bc068b59e33a966ae85b973dfd1e22f5b75
Ben Ross <mrbenross@gmail.com>
2023-12-08 17:32:51 -0600

    commit

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,12 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import Spinner from "./Spinner"
+import {render,screen} from '@testing-library/react'
+import React from "react";
 test('sanity', () => {
-  expect(true).toBe(false)
+ // expect(true).toBe(false)
+ render(<Spinner on={false} />);
+ const spinnerElement = screen.queryByTestId('spinner'); // Use an appropriate test ID
+
+ expect(spinnerElement).toBeNull();
 })
